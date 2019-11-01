60c0df3264ead31f798f18c527bfdfb7b448a8cf
yymirr <abla.ue@web.de>
2019-11-01 15:05:01 +0100

    removed the wrong dbQueries.js file

--- dbQueries.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-
-module.exports.checkUserAndPassword = function(request ,username, password) {
-    return request.app.get('db').prepare('SELECT * FROM users WHERE username = ? AND password = ?').get(username,password) 
-}
-
