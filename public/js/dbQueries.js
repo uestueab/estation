256d794b28538fdd0f086b792b6c8fb210a809c7
yymirr <abla.ue@web.de>
2019-11-02 22:33:05 +0100

    commented out some queries which might be useful in the future

--- public/js/dbQueries.js
+++ public/js/dbQueries.js
@@ -5,3 +5,26 @@ module.exports.checkUserAndPassword = function(request ,username, password) {
     return request.app.get('db').prepare('SELECT * FROM users WHERE username = ? AND password = ?').get(username,password) 
 }
 
+
+
+// populate db with dummy users
+// db.prepare('CREATE TABLE IF NOT EXISTS users'
+// + '(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)').run();
+// const insert = db.prepare('INSERT INTO users (username, password) VALUES ("admin", "admin")').run();
+
+// const insert = db.prepare('INSERT INTO friends (name, age) VALUES (@name, @age)');
+
+// const insertMany = db.transaction((friends) => {
+// for (const friend of friends) insert.run(friend);
+// });
+
+// insertMany([
+// { name: 'Donald', age: 23 },
+// { name: 'Micky', age: 24 },
+// { name: 'Goofy', age: 25 },
+// ]);
+
+// const getUserFromDB = db.transaction((user) => {
+// stmtGetUser.get(user).username;
+// });
+
