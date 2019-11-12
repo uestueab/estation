43af879a8cc65985365be08f2b94b8d39f3ced20
yymirr <abla.ue@web.de>
2019-11-12 11:20:30 +0100

    giant insert dump, values of column kraftstofftyp oft table tankstellen are random

---
+++ db/insertUsers.sql
@@ -0,0 +1,4 @@
+INSERT INTO "users"(id,username,password) VALUES(1,'admin','password');
+INSERT INTO "users"(id,username,password) VALUES(2,'dummy','sometext');
+INSERT INTO "users"(id,username,password) VALUES(5,'donald','duck');
+INSERT INTO "users"(id,username,password) VALUES(6,'mickey','mouse');
